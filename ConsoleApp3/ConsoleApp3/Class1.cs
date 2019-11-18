1ffd5b7af1516b32fbe7026476302f1bfe4991f4
Michael Carnine <test.carnine@gmail.com>
2019-11-18 14:36:08 -0600

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VariableDefinition
+{
+    
+    class Program
+    {
+        static String userName;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter User Name"); 
+            
+            userName = GetUserName();
+            Console.WriteLine(userName);
+
+            userName = SetUserName("Mike");
+            Console.WriteLine(userName);
+
+            Console.Read();
+        }
+
+        public static String GetUserName()
+        {
+            userName = Convert.ToString(Console.ReadLine());
+            return userName;
+        }
+
+        public static String SetUserName(String myName) { 
+            return myName;
+        }
+    }
+}
